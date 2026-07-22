1da65dcd9bf8bca8d5d6e9b34d17bc4e48ed5b50
bowtec95 <bowtec95@gmail.com>
2026-07-21 20:06:20 -0400

    Update GripperHealthApp.py
    line 183 gripper layout

--- GripperHealthApp.py
+++ GripperHealthApp.py
@@ -178,10 +178,7 @@ def show_gripper_layout():
     for row in range(rows):
         for col in range(cols):
 
-            num = (
-                (rows - row) +
-                ((cols - 1 - col) * rows)
-            )
+           num = ((rows - 1 - row) * cols) + col + 1
 
             color = (
                 st.session_state.module_colors.get(
